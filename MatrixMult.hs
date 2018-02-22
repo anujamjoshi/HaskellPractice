101e7d14f902d93bbd5f865c4f1a0daa2a313893
Anuja Joshi <anuja.j20@gmail.com>
2018-02-22 02:30:01 -0800

    Matrix multiplication in Haskell

--- MatrixMult.hs
+++ MatrixMult.hs
@@ -4,7 +4,15 @@ import Formatting.Clock
 import System.Clock
 import Data.List
 
+
+--utilizing the function tranpose that comes with the List import 
+-- this function takes in two parameters the matrix a and b and returns the product of a and b 
+mmult:: Num a => [[a]] -> [[a]] -> [[a]]
+mmult a _ = []
+mmult _ b = []
 mmult a b = [ [ sum $ zipWith (*) ar bc | bc <- (transpose b) ] | ar <- a ]
+
+
 main =
   do start <- getTime Monotonic
      -- print $[[1, 2],[3, 4]] `mmult` [[-3, -8, 3], [-2,  1, 4]]
