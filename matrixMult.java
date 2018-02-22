e1a05cbf2a20459350b25bc0afc2ea7ba5251173
tanyaw <twanwatanakool@gmail.com>
2018-02-21 17:47:24 -0800

    Completed C++ matrix multiplication.

--- matrixMult.java
+++ matrixMult.java
@@ -38,44 +38,13 @@
       }
 
       double avgTime = totalTime/10.0;
-      System.out.println("Average time taken: " + avgTime + " nanoseconds.");
+      System.out.println("Java Matrix Mult Average time taken: " + avgTime + " nanoseconds.");
    }
-
-
-   /**
-   * This is the matrix multiplication method on various matrix sizes.
-   * @param double a[m][n] and double b[n][p]
-   * @return double[m][p]
-   */
-   public static double[][] mult2(double a[][], double b[][]){
-      if(a.length == 0) {
-         return new double[0][0];
-      }
-      if(a[0].length != b.length) {
-         return null; //invalid dims
-      }
-    
-      int n = a[0].length;
-      int m = a.length;
-      int p = b[0].length;
-    
-      double result[][] = new double[m][p];
-    
-      for (int i = 0; i < m; i++) {
-         for (int j = 0; j < p; j++) {
-            for (int k = 0; k < n; k++) {
-               result[i][j] += a[i][k] * b[k][j];
-            }
-         }
-      }
-      return result;
-   }
-
 
    /**
    * This is the matrix multiplication method on nxn matrices.
-   * @param double a[n][n] and double b[n][n]
-   * @return double[n][n]
+   * @param double a[][], double b[][] Two matrices.
+   * @return The product of the two matrices.
    */
    public static double[][] mult(double a[][], double b[][]){
       if(a.length == 0) {
@@ -101,8 +70,8 @@
 
 
    /**
-   * This is the matrix multiplication method on various matrix sizes.
-   * @param double a[m][n]
+   * This method turns the matrix into a printable value
+   * @param double a[][] A matrix.
    * @return String 
    */
    public static String toString(double a[][]) {
